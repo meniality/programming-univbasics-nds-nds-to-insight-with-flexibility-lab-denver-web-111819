e82813910abe65a8dc1032beb6932fc2272dfa91
patrick hoehn <patrh_99@hotmail.com>
2019-11-12 16:27:24 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -34,6 +34,8 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
+  
+end
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
@@ -48,7 +50,7 @@ def movies_with_director_key(name, movies_collection)
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
-end
+
 
 
 def gross_per_studio(collection)
