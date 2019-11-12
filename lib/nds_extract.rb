d3b46f7bd79d860b832f7a9da8e0742eab75aa66
patrick hoehn <patrh_99@hotmail.com>
2019-11-12 16:33:52 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -40,6 +40,7 @@ def movies_with_director_key(name, movies_collection)
   while i < movies_collection.length do
     movie_data = movies_collection[i]
     movies_and_directors_array.push(movie_with_director_name(name, movie_data))
+    i += 1
   end
   movies_and_directors_array
 end
